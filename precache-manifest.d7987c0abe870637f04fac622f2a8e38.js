2e868940da81a620de96cbd3608839e04dea26bf
Freedom_Natsumi <freedomnatsumi@gmail.com>
2020-07-17 22:27:17 +0800

    Updates

---
+++ precache-manifest.d7987c0abe870637f04fac622f2a8e38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4296bf78dd43c28b60e91d4776026930",
+    "url": "/own-website/index.html"
+  },
+  {
+    "revision": "4758abde7e19a2a258e4",
+    "url": "/own-website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "652a599d1a99efccd345",
+    "url": "/own-website/static/js/2.a430f49c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/own-website/static/js/2.a430f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4758abde7e19a2a258e4",
+    "url": "/own-website/static/js/main.5c723c10.chunk.js"
+  },
+  {
+    "revision": "49f9e3007c422156223d",
+    "url": "/own-website/static/js/runtime-main.af36a617.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/own-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
